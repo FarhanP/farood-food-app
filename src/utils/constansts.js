7cc7b62e66820ab30f3d149be0941651eb4e5153
farhancodes <pmdfarhan@gmail.com>
2025-01-01 05:12:14 +0530

    Update constansts.js

--- src/utils/constansts.js
+++ src/utils/constansts.js
@@ -1,4 +1,4 @@
-export const LOGO_URL = "https://farood-food-app.vercel.app/api/logo";
+export const LOGO_URL = "https://farood-food-app.vercel.app/api/logo/";
 
 export const MENU_API =
   "https://farood-food-app.vercel.app/api/menu&restaurantId=";
