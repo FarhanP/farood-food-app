d75969696685b677c226546f1a2b8e450b9a960d
farhancodes <pmdfarhan@gmail.com>
2025-01-01 05:14:42 +0530

    url image updates

--- src/utils/constansts.js
+++ src/utils/constansts.js
@@ -1,4 +1,5 @@
-export const LOGO_URL = "https://farood-food-app.vercel.app/api/logo/";
+export const LOGO_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,c_fill/";
 
 export const MENU_API =
   "https://farood-food-app.vercel.app/api/menu&restaurantId=";
