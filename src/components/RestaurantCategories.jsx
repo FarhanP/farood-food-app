37a23aba7f2ece45a9e3a03e9264fb303722807d
farhancodes <pmdfarhan@gmail.com>
2025-01-01 06:15:45 +0530

    Update RestaurantCategories.jsx

--- src/components/RestaurantCategories.jsx
+++ src/components/RestaurantCategories.jsx
@@ -1,20 +1,22 @@
 import ItemsList from "./ItemsList";
 
-
-const RestaurantCategories = ({showItems, setShowIndex, data }) => {
-   const handleClick = ()=>{
-     setShowIndex();
-   }
+const RestaurantCategories = ({ showItems, setShowIndex, data }) => {
+  const handleClick = () => {
+    setShowIndex();
+  };
 
   return (
     <div>
       {/* {Accordiion Title} */}
       <div className="sm:w-6/12  m-auto my-4 p-4 shadow-lg bg-themePink">
-        <div className="flex justify-between cursor-pointer" onClick={handleClick}>
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
           <span className=" text-xl font-bold sm:text-base">
             {data.title} ({data.itemCards.length})
           </span>
-          <span className="text-xl sm:text-base">⬇</span>
+          <span className="text-lg sm:text-base">⬇</span>
         </div>
         {/* {Accordion body} */}
         {showItems && <ItemsList items={data.itemCards} />}
