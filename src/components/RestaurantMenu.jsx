0be6ea3f6a7e58f10907d86425b19e328b315982
farhancodes <pmdfarhan@gmail.com>
2025-01-03 00:47:58 +0530

    Accordion toggle issue fix

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -14,12 +14,13 @@ const RestaurantMenu = () => {
   const { name, cuisines, costForTwo, avgRating } =
     resMenu?.cards[2]?.card?.card?.info;
 
-  const responseCard = window.innerWidth <= 768 ? resMenu?.cards[5] : resMenu?.cards[4];
+  const responseCard =
+    window.innerWidth <= 768 ? resMenu?.cards[5] : resMenu?.cards[4];
 
   const { itemCards } =
-   responseCard?.groupedCard?.cardGroupMap?.REGULAR?.cards[2].card.card;
+    responseCard?.groupedCard?.cardGroupMap?.REGULAR?.cards[2].card.card;
   const categories =
-  responseCard?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+    responseCard?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (card) =>
         card.card?.card["@type"] ==
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
@@ -29,12 +30,12 @@ const RestaurantMenu = () => {
     <div className="menu m-3 p-3 text-center">
       <h1 className="font-bold text-3xl sm:text-2xl my-8 sm:my-6">{name}</h1>
       <p className=" text-xl text-bold sm:text-lg">
-        {cuisines.join(", ")} - {costForTwo/100}
+        {cuisines.join(", ")} - {costForTwo / 100}
       </p>
       {/* {Categories Accordion} */}
       {categories.map((category, index) => (
         <RestaurantCategories
-          showItems={index === showIndex ? true : false}
+          showItems={index === showIndex && showIndex === false ? true : false}
           setShowIndex={() => setShowIndex(index)}
           data={category?.card?.card}
           key={category?.card?.card?.title}
