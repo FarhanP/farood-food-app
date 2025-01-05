83367346baa7675359213627534d408d4768fb41
farhancodes <pmdfarhan@gmail.com>
2025-01-05 21:29:23 +0530

    Added Linkedin option

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
   return (
     <div className="text-center m-4 p-4">
       <div className="font-bold text-3xl sm:text-2xl my-8 sm:my-6">
-        Contact Us
+        Contact Me
       </div>
       <div className="text-xl sm:text-2xl flex sm:w-6/12 sm:m-auto  flex-col text-themePurple">
         <a
@@ -29,7 +33,16 @@ const Contact = () => {
         </a>
         <a
           className="font-bold my-6 px-5 py-2 rounded bg-themePurple text-white"
-          href="https://github.com/FarhanP"
+          href="https://www.linkedin.com/in/farhanp/"
+          target="_tab"
+          role="button"
+        >
+          LinkedIn
+          <FontAwesomeIcon className="px-2" icon={faLinkedin} />
+        </a>
+        <a
+          className="font-bold my-6 px-5 py-2 rounded bg-themePurple text-white"
+          href="https://x.com/mfarhancodes"
           target="_tab"
           role="button"
         >
