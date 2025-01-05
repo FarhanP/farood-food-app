3beaf6a394fba6e94d0650dafe90a31c75eb3cd1
farhancodes <pmdfarhan@gmail.com>
2025-01-05 21:25:09 +0530

    New design for contact us page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,41 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSchool } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+
 const Contact = () => {
   return (
     <div className="text-center m-4 p-4">
       <div className="font-bold text-3xl sm:text-2xl my-8 sm:my-6">
         Contact Us
       </div>
-      <div className="text-xl sm:text-2xl w-auto text-themePurple">
-        <div className="email my-6 px-2">
-          Email:<span className="font-bold px-2">pmdfarhan@gmail.com</span>
-        </div>
-        <div className="my-6">
-          <a
-            className="font-bold text-xl px-3 py-2 rounded bg-themePurple text-white"
-            href="https://github.com/FarhanP"
-            target="_self"
-            role="button"
-          >
-            Github
-            <FontAwesomeIcon className="px-2" icon={faGithub} />
-          </a>
-        </div>
+      <div className="text-xl sm:text-2xl flex sm:w-6/12 sm:m-auto  flex-col text-themePurple">
+        <a
+          className="font-bold my-6 px-5 py-2 rounded bg-themePurple text-white"
+          href="mailto:pmdfarhan@example.com"
+          target="_tab"
+          role="button"
+        >
+          Email
+          <FontAwesomeIcon className="px-2" icon={faEnvelope} />
+        </a>
+        <a
+          className="font-bold my-6 px-5 py-2 rounded bg-themePurple text-white"
+          href="https://github.com/FarhanP"
+          target="_tab"
+          role="button"
+        >
+          Github
+          <FontAwesomeIcon className="px-2" icon={faGithub} />
+        </a>
+        <a
+          className="font-bold my-6 px-5 py-2 rounded bg-themePurple text-white"
+          href="https://github.com/FarhanP"
+          target="_tab"
+          role="button"
+        >
+          X (Twitter)
+          <FontAwesomeIcon className="px-2" icon={faXTwitter} />
+        </a>
       </div>
     </div>
   );
